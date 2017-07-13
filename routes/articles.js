1e9c6f0fe62ae2b0cb9b0f9749fb2ce9f8eeaea2
Fabricio Bezerra <demonstro003@gmail.com>
2017-07-13 18:58:22 -0300

    Node.js & Express From Scratch [Part 6]

--- routes/articles.js
+++ routes/articles.js
@@ -1,10 +1,39 @@
 const express = require('express');
-var router = express.Router();
+const mongoose = require('mongoose');
+let router = express.Router();
+let Article = require('../models/article');
 
-/* GET home page. */
+/* GET add article page. */
 router.get('/add', function(req, res, next) {
   res.render('articles/add', {
     title: 'Add Article'
+  });
+});
+
+/* POST add article */
+router.post('/add', function(req, res, next) {
+  let article = new Article({
+    title: req.body.title,
+    author: req.body.author,
+    body: req.body.body
+  });
+
+  article.save((err) => {
+    if (err) {
+      console.error(err);
+      return;
+    } else {
+      res.redirect('/');
+    }
+  });
+});
+
+// Get single article
+router.get('/:id', function(req, res) {
+  Article.findById(req.params.id, (err, article) => {
+    res.render('articles/article', {
+      article: article
+    });
   });
 });
 
